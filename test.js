b2a97d44310950794cb30f746a4191852fc6de74
MatthewZGong <matthewzgong@gmail.com>
2021-06-08 15:48:18 -0700

    removed

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var req = unirest("POST", "https://shazam.p.rapidapi.com/songs/detect");
 
 req.headers({
 	"content-type": "text/plain",
-	"x-rapidapi-key": "7e3bb9c2cfmsh31c1e508834003dp175d3djsn29e094e0f8f9",
+	"x-rapidapi-key": 
 	"x-rapidapi-host": "shazam.p.rapidapi.com",
 	"useQueryString": true
 });
@@ -13,6 +13,7 @@ req.send("I want it that way");
 
 
 req.end(function (res) {
+
 
 	console.log(res.body);
 });
